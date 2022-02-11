d50b317738913e27f7af86054535d6e477445af9
stanislaumich <staslz42stas@gmail.com>
2022-02-11 09:33:50 +0300

    Start!

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,18 @@
 #include <Arduino.h>
+// https://adior.ru/index.php/robototekhnika/249-pcint-arduino
+
+
+#define LED 0
 
 void setup() {
-  // put your setup code here, to run once:
+  // initialize digital pin LED_BUILTIN as an output.
+  pinMode(LED, OUTPUT);
 }
 
+// the loop function runs over and over again forever
 void loop() {
-  // put your main code here, to run repeatedly:
+  digitalWrite(LED, HIGH);   // turn the LED on (HIGH is the voltage level)
+  delay(1000);                       // wait for a second
+  digitalWrite(LED, LOW);    // turn the LED off by making the voltage LOW
+  delay(1000);                       // wait for a second
 }
